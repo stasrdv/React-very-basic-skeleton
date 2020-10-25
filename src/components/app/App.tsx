7e4d162d116637ed4fc6b1defe46d9d1dbea60b2
stasrdv <stasrdv@gmail.com>
2020-10-25 16:55:55 +0200

    Very basic UI skelaton implemented.

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,40 +1,16 @@
 import React from "react";
 import "./App.scss";
-import Footer from '../footer/footer'
-import { makeStyles } from "@material-ui/core/styles";
+import Footer from "../footer/footer";
+import Header from "../header/header";
 
-
-
-const useStyles = makeStyles({
-  wrapper: {
-    display: "flex",
-    flexFlow: "column",
-    height:"100%",
-   
-  },
-  contentMain:{
-    flex:"1",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor:"#f9f9f9"
-  }
-});
-
-
-const App = ()=> {
-  const classNamees = useStyles();
+const App = () => {
   return (
-    <div className={classNamees.wrapper}>
-      <header  className="header"> React on my way
-      </header>
-      <div className={classNamees.contentMain}>
-
-      </div>
-    <Footer />
+    <div className="main__wrapper">
+      <Header />
+      <div className="main__content"></div>
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
-
---
+++ src/components/header/header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./header.scss";
+
+
+const Header = () => {
+  return <header className="header"> React on my way</header>;
+};
+
+export default Header;
