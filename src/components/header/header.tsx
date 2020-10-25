7651ad57d2707eb876d1da23323bdb23b8fdf299
stasrdv <stasrdv@gmail.com>
2020-10-25 17:11:16 +0200

    Small refactoring.

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import "./header.scss";
-
+import logo from "../../assets/svg/logo.svg";
 
 const Header = () => {
-  return <header className="header"> React on my way</header>;
+  return (
+    <header className="header">
+      <img src={logo} className="header__logo" alt="logo" />
+      <div className="header__item">React On My Way</div>
+    </header>
+  );
 };
 
 export default Header;
